a5731bbb02bc27b203da704ca339b47f58689612
Bruno Barreto <bruno.ads.ti@gmail.com>
2022-11-03 21:04:14 -0300

    Commit com a versão incial do projeto de Criar um Sistema e Abstrair um Celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia1 = new Nokia("4567689", "Modelo2", "333333335", 32);
+nokia1.InstalarAplicativo("Instagram");
+nokia1.Ligar();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone1 = new Iphone("57495", "Modelo3", "333333334", 64);
+iphone1.InstalarAplicativo("Spotify");
+iphone1.ReceberLigacao();
+
+
+
+
